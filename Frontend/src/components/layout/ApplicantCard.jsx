c217842ae7df45e185b2bbd7125008ec7fcc4bb3
Dipika Gandash <dipikadevv1445@gmail.com>
2026-03-13 20:45:29 +0530

    implement applicant status update for recruiters

--- Frontend/src/components/layout/ApplicantCard.jsx
+++ Frontend/src/components/layout/ApplicantCard.jsx
@@ -1,4 +1,6 @@
-import React from "react";
+import api from "@/api/axios";
+import React, { useState } from "react";
+import toast from "react-hot-toast";
 
 const statusColors = {
   Applied: "bg-blue-100 text-blue-700",
@@ -9,19 +11,44 @@ const statusColors = {
 
 const ApplicantCard = ({ app }) => {
   const { user, status } = app;
-//   const getResumeViewUrl = (resumeUrl) => {
-//   return `https://docs.google.com/viewer?url=${encodeURIComponent(resumeUrl)}&embedded=false`;
-// };
+  const [applicantStatus, setApplicantStatus] = useState(status);
+  const [selectedStatus, setSelectedStatus] = useState(status);
+
+  const handleUpdateStatus = async (applicationId) => {
+    try {
+      await api.patch(`/api/v1/application/${applicationId}/status`, {
+        status: selectedStatus,
+      });
+      toast.success("Status updated !");
+      setApplicantStatus(selectedStatus);
+    } catch (error) {
+      toast.error(error.response?.data?.message || "Something went wrong");
+    }
+  };
 
   return (
-    <div className="p-6 rounded-xl shadow-md bg-white flex justify-between items-start gap-4">
-      
+    <div className="p-6 rounded-xl shadow-md bg-white gap-4">
       <div className="flex flex-col gap-2 flex-1">
-        <div className="flex items-center gap-3">
-          <h2 className="text-lg font-semibold text-gray-800">
-            {user.firstName} {user.lastName}
-          </h2>
-          <span className="text-sm text-gray-500">{user.phoneNumber}</span>
+        <div className="flex justify-between gap-3">
+          <div className="flex flex-col md:flex-row md:gap-4">
+            <h2 className="text-lg font-semibold text-gray-800">
+              {user.firstName} {user.lastName}
+            </h2>
+            <span className="text-sm text-gray-600">{user.phoneNumber}</span>
+          </div>
+
+          {user.profile?.resume ? (
+            <a
+              href={user.profile.resume}
+              target="_blank"
+              rel="noopener noreferrer"
+              className="text-sm text-indigo-600 hover:underline font-medium"
+            >
+              View Resume
+            </a>
+          ) : (
+            <span className="text-sm text-gray-400">No resume uploaded</span>
+          )}
         </div>
 
         <div className="flex flex-wrap gap-2">
@@ -38,34 +65,33 @@ const ApplicantCard = ({ app }) => {
             ))
           )}
         </div>
-
-        {user.profile?.resume ? (
-        <a
-     href={user.profile.resume}
-    target="_blank"
-    rel="noopener noreferrer"
-    className="text-sm text-indigo-600 hover:underline font-medium"
->
-  View Resume
-</a>
-        ) : (
-          <span className="text-sm text-gray-400">No resume uploaded</span>
-        )}
       </div>
 
-      <div className="flex flex-col items-end gap-3">
-        <span className={`text-xs font-medium px-3 py-1 rounded-full ${statusColors[status]}`}>
-          {status}
+      <div className="flex items-start md:items-end mt-5 gap-3">
+        <span
+          className={`text-sm font-medium px-3 py-1 md:px-4 md:py-2 rounded-full ${statusColors[status]}`}
+        >
+          {applicantStatus}
         </span>
 
-        <select className="text-sm border border-gray-300 rounded-lg px-2 py-1 focus:outline-none focus:ring-2 focus:ring-indigo-500">
+        <select
+          className="text-sm border border-gray-600 rounded-lg px-2 py-1 focus:outline-none focus:ring-2 focus:ring-indigo-500"
+          value={selectedStatus}
+          onChange={(e) => setSelectedStatus(e.target.value)}
+        >
           <option value="Applied">Applied</option>
           <option value="Shortlisted">Shortlisted</option>
           <option value="Rejected">Rejected</option>
           <option value="Hired">Hired</option>
         </select>
-      </div>
 
+        <button
+          onClick={() => handleUpdateStatus(app._id)}
+          className="text-sm bg-indigo-600 text-white px-3 py-1  md:px-4 md:py-2 rounded-lg hover:bg-indigo-700"
+        >
+          Update
+        </button>
+      </div>
     </div>
   );
 };
